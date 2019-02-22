83e8a3e0014fdfba6540cd5131088c860ea44ab7
TM Lee <tmlee.ltm@gmail.com>
2019-02-22 22:55:53 +0800

    Cleanup landing no auth

--- src/components/Landing/Landing.vue
+++ src/components/Landing/Landing.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="row q-px-md">
-    <q-card class="col col-12">
+  <div class="row q-px-md q-mt-lg d-flex justify-center">
+    <q-card class="col col-3">
       <div class="col col-12 q-pa-lg text-center">
-        <span class="text-h6">
-          Connect with MetaMask to get started
-        </span>
-        <div class="text-center q-py-lg">
+        <div class="text-center q-pt-lg">
           <a href="https://metamask.io" rel="noreferrer" target="_blank">
             <img
               class="Metamask__Img"
@@ -15,6 +12,7 @@
             />
           </a>
         </div>
+        <div class="text-h7 q-pb-lg">Connect with MetaMask to get started</div>
         <div class="text-center">
           <q-btn
             class="q-pa-md"
